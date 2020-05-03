06bb70a82aa9893a7c2f6e83c1defd5535f1d8f9
= <=>
2020-05-03 10:41:53 +0700

    Updates

---
+++ precache-manifest.5d0c9db07b3888a4c385dd4febb217dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596e5e30bff46252bb5d32f84bb844bb",
+    "url": "/giuaky/index.html"
+  },
+  {
+    "revision": "f431440ace6755b7457e",
+    "url": "/giuaky/static/css/main.d12e4dd6.chunk.css"
+  },
+  {
+    "revision": "50a635a3193a4c6bd2a1",
+    "url": "/giuaky/static/js/2.b61d46c1.chunk.js"
+  },
+  {
+    "revision": "0444e8a79776ad1c5dd468972b4f1bd9",
+    "url": "/giuaky/static/js/2.b61d46c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f431440ace6755b7457e",
+    "url": "/giuaky/static/js/main.17bcae63.chunk.js"
+  },
+  {
+    "revision": "a469fb66bfcb1326e310",
+    "url": "/giuaky/static/js/runtime-main.b08c0cbe.js"
+  }
+]);
